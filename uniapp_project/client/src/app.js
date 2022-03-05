641ebb806d670fa132e0930c96117db98957027d
cgaona1 <gaonachris930@gmail.com>
2022-03-05 14:43:36 -0700

    Added styles for that are going to be used for app.js and applied them

--- uniapp_project/client/src/app.js
+++ uniapp_project/client/src/app.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
 import Questions from './components/Questions/questions.js';
-import Form from './components/Form/form.js'
+import Form from './components/Form/form.js';
 import UniAppImage from './images/UniAppImage.jpeg';
+import useStyles from './styles.js';
 
 const App = () => {
+    const classes = useStyles();
+
     return (
         <Container maxWidth='lg'>
-            <AppBar position='static' color='inherit'>
-                <Typography variant='h2' align='center'>Uni App</Typography>
-                <img src={UniAppImage} alt='UniApp' height='60'/>
+            <AppBar className={classes.appBar} position='static' color='inherit'>
+                <Typography className={classes.heading} variant='h2' align='center'>Uni App</Typography>
+                <img className={classes.image} src={UniAppImage} alt='UniApp' height='60'/>
             </AppBar>
             <Grow in>
                 <Container>
