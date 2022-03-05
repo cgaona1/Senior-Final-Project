1d54aba9c4880d855185d31c9b089a0f4bd2303d
cgaona1 <gaonachris930@gmail.com>
2022-03-04 20:34:39 -0700

    Added files for components and updated app.js

--- uniapp_project/client/src/app.js
+++ uniapp_project/client/src/app.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import UniAppImage from './images/UniAppImage.jpeg';
 
 const App = () => {
     return (
-        <div>
-            <h1>Hell UniApp</h1>
-        </div>
+        <Container maxWidth='lg'>
+            <AppBar position='static' color='inherit'>
+                <Typography variant='h2' align='center'>Uni App</Typography>
+                <img src={UniAppImage} alt='UniApp' height='60'/>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify='space-between' alignItems='stretch' spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Questions />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     );
 }
 
