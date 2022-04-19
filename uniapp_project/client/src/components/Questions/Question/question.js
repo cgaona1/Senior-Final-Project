44e460f8755353932e09c183c371b04801ebe6a0
cgaona1 <gaonachris930@gmail.com>
2022-04-19 17:47:28 -0600

    Added styles for the components

--- uniapp_project/client/src/components/Questions/Question/question.js
+++ uniapp_project/client/src/components/Questions/Question/question.js
@@ -1,6 +1,9 @@
 import React from "react";
+import useStyles from "./styles.js";
 
 const Question = () => {
+    const classes = useStyles();
+
     return (
         <h1>QUESTION</h1>
     );
--- uniapp_project/client/src/components/Questions/questions.js
+++ uniapp_project/client/src/components/Questions/questions.js
@@ -1,7 +1,10 @@
 import React from "react";
 import Question from "./Question/question.js";
+import useStyles from "./styles.js";
 
 const Questions = () => {
+    const classes = useStyles();
+
     return (
         <>
             <h1>QUESTIONS</h1>
