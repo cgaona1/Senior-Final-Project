5089e0bff3978f09cdd43b25cf8a6e7b80e7e0e7
cgaona1 <gaonachris930@gmail.com>
2022-05-06 00:11:03 -0600

    Questions display from the database

--- uniapp_project/client/src/components/Questions/Question/question.js
+++ uniapp_project/client/src/components/Questions/Question/question.js
@@ -1,12 +1,41 @@
 import React from "react";
 import useStyles from "./styles.js";
+import moment from 'moment';
 
-const Question = () => {
+import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@material-ui/core/";
+
+const Question = ({ question }) => {
     const classes = useStyles();
 
     return (
-        <h1>QUESTION</h1>
+        <Card className={classes.card}>
+            <CardMedia className={classes.media} title={question.questionTitle}/>
+            <div className={classes.overlay}>
+                <Typography variant="h6">{question.questionCreator}</Typography>
+                <Typography variant="body2">{moment(question.createdAt).fromNow()}</Typography>
+            </div>
+            <div className={classes.overlay2}>
+                <Button style={{color: 'white'}} size="small" onClick={() => {}}>
+                </Button>
+            </div>
+            <div className={classes.details}>
+                <Typography variant="body2" color="textSecondary">{question.questionTags.map((tag) => `#${tag}`)}</Typography>
+            </div>
+            <Typography className={classes.title} variant="h5" gutterBottom component="h2">{question.questionTitle}</Typography>
+            <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">{question.questionMessage}</Typography>
+            </CardContent>
+            <CardActions className={classes.cardActions}>
+                <Button size="small" color="primary" onClick={() => {}}>
+                    Up Vote
+                    {question.upVoteCount}
+                </Button>
+                <Button size="small" color="primary" onClick={() => {}}>
+                    Delete
+                </Button>
+            </CardActions>
+        </Card>
     );
-}
+};
 
 export default Question;
--- uniapp_project/client/src/components/Questions/questions.js
+++ uniapp_project/client/src/components/Questions/questions.js
@@ -3,6 +3,7 @@ import Question from "./Question/question.js";
 import { useSelector } from "react-redux";
 
 import useStyles from "./styles.js";
+import { CircularProgress, Grid } from "@material-ui/core";
 
 const Questions = () => {
     const questions = useSelector((state) => state.questions);
@@ -11,12 +12,16 @@ const Questions = () => {
     console.log(questions);
 
     return (
-        <>
-            <h1>QUESTIONS</h1>
-            <Question />
-            <Question />
-        </>
+        !questions.length ? <CircularProgress/> : (
+            <Grid className={classes.mainContainer} container spacing={2} alignItems="stretch">
+                {questions.map((question) => (
+                    <Grid key={question._id} item xs={12} sm={6}>
+                        <Question question={question}/>
+                    </Grid>
+                ))}
+            </Grid>
+        )
     );
-}
+};
 
 export default Questions;
